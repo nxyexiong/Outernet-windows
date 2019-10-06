6d611d3ab4c2774cb05a0f2daa1b46c60fa34e4b
nxyexiong <nxyexiong@gmail.com>
2019-10-06 18:15:38 +0800

    stop order

--- main.py
+++ main.py
@@ -116,7 +116,6 @@ class MainControl:
     def handle_stop(self):
         print("terminating...")
         self.running = False
-        self.sys_hper.uninit_network(self.server_ip)
         if self.tap_control is not None:
             self.tap_control.close()
         if self.client is not None:
@@ -126,6 +125,7 @@ class MainControl:
         self.tap_control = None
         self.tuntap = None
         self.client = None
+        self.sys_hper.uninit_network(self.server_ip)
         print("terminated")
         if self.stop_cb is not None:
             self.stop_cb()
