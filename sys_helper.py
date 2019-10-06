a6598e5297c5267b8e990a2fde8fe8bd256e2d3b
nxyexiong <nxyexiong@gmail.com>
2019-10-06 18:01:53 +0800

    modify route metric

--- sys_helper.py
+++ sys_helper.py
@@ -4,17 +4,10 @@ import subprocess
 
 from iface_helper import get_default_iface, get_iface_gateway_ipv4, get_iface_name, get_tap_iface
 
-NET_CONFIG_FILE = 'network_config.dat'
-
 
 def execute(cmd):
     CREATE_NO_WINDOW = 0x08000000
     subprocess.call(cmd, creationflags=CREATE_NO_WINDOW)
-
-
-def execute_with_shell(cmd):
-    CREATE_NO_WINDOW = 0x08000000
-    subprocess.call(cmd, creationflags=CREATE_NO_WINDOW, shell=True)
 
 
 class SysHelper:
@@ -31,13 +24,8 @@ class SysHelper:
         ipv4_network = '.'.join([str(item) for item in ipv4_network])
         ipv4_netmask = '.'.join([str(item) for item in ipv4_netmask])
 
-        # save network config
-        dirname = os.path.abspath(os.path.dirname(sys.argv[0]))
-        execute_with_shell("netsh interface dump > %s\\%s" % (dirname, NET_CONFIG_FILE))
-
-        # delete metric=0
-        execute("route delete 0.0.0.0 metric 25")
-        execute("route delete %s metric 25" % (server_addr,))
+        # set metric!=0
+        execute("netsh interface ipv4 set route 0.0.0.0/0 %s metric=100" % (self.default_ifname))
 
         # setup interface
         execute("netsh interface ip set address %s static %s %s" % (self.tap_ifname, ipv4_addr, ipv4_netmask))
@@ -64,10 +52,6 @@ class SysHelper:
         # delete tap route
         execute("netsh interface ipv4 delete route 0.0.0.0/0 %s" % (self.tap_ifname,))
 
-        # restore network config
-        dirname = os.path.abspath(os.path.dirname(sys.argv[0]))
-        execute("netsh exec %s\\%s" % (dirname, NET_CONFIG_FILE))
-
     def install_tap():
         dirname = os.path.abspath(os.path.dirname(sys.argv[0]))
         execute("%s\\tap\\tapinstall.exe install %s\\tap\\OemVista.inf tap0901" % (dirname, dirname))
@@ -77,9 +61,8 @@ class SysHelper:
         execute("%s\\tap\\tapinstall.exe remove tap0901" % (dirname,))
 
     def fix_netsh():
-        dirname = os.path.abspath(os.path.dirname(sys.argv[0]))
-        execute("netsh exec %s\\%s" % (dirname, NET_CONFIG_FILE))
         execute("netsh winsock reset")
+        execute("route -f")
 
     def restart_pc():
-        execute("shutdown /r")
+        execute("shutdown /r /t 0")
